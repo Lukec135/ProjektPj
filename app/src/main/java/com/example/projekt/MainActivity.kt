372d77cbe720c98c912168475c8e49cc541ff35d
Ziga Adam <ziga.adam@gmail.com>
2022-05-15 21:35:53 +0200

    Žiga 3.commit: - Klic APIja - dobljen Json iz apija - dekodiran json v string(base64) - !POTREBNO VSEBINO SPREMENITI V FILE (.ZIP)!

--- app/src/main/java/com/example/projekt/MainActivity.kt
+++ app/src/main/java/com/example/projekt/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.projekt
 
 
-import android.app.Activity
+
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projekt.databinding.ActivityMainBinding
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.create
+import org.json.JSONObject
+import java.io.File
+import java.io.IOException
+import java.nio.charset.StandardCharsets.UTF_8
+import java.util.*
+import java.util.zip.GZIPInputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 
         val getData =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
+                if (result.resultCode == RESULT_OK) {
                     val data: Intent? = result.data
 
                     //izpise vsebino qr kode
@@ -49,6 +60,84 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+
+        binding.test123.setOnClickListener{
+            post("https://api-ms-stage.direct4.me/sandbox/v1/Access/openbox","{\n" +
+                    "\"boxId\": 352,\n" +
+                    "\"tokenFormat\": 2\n" +
+                    "}")
+        }
+
+
     }
 
+
+    val token = "9ea96945-3a37-4638-a5d4-22e89fbc998f"
+
+    val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
+
+    var client = OkHttpClient()
+
+    @Throws(IOException::class)
+    fun post(url: String, json: String) {
+        val body: RequestBody = create(JSON, json)
+        val request: Request = Request.Builder()
+            .url(url)
+            .addHeader("Authorization", "Bearer " + token)
+            .post(body)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("NAPAKA")
+                e.printStackTrace()
+                println("Konec Erorja")
+            }
+
+
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        println("NAPAKA2")
+                        throw IOException("Unexpected code $response")
+                    }
+                    println("PRED KONZOLO")
+                    for ((name, value) in response.headers) {
+                        println("|$name|: |$value|")
+                    }
+
+                    //println(response.body!!.string())
+
+                    //var respondeBody = JSONObject(Objects.requireNonNull(response.body).toString())       //NESMES 2x izpisat bodyja
+                    var respondeBody = JSONObject(response.body!!.string())
+
+                    //println("TOJETOKENKODIRAN:"+ respondeBody.getString("data"))
+
+                    val tokenInBase64 = respondeBody.getString("data")
+
+                    val decoder: Base64.Decoder = Base64.getDecoder()
+                    val tokenDecoded = String(decoder.decode(tokenInBase64))
+
+                    //println("TOJETOKENDECODE:"+tokenDecoded)
+                    println("TEST1")
+
+                    //File("testniStringZipan").writeText(tokenDecoded)
+                    decoder(tokenInBase64,"FIleZaUporabo.zip")
+
+
+                    println("TEST2")
+                }
+            }
+        })
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun decoder(base64Str: String, pathFile: String): Unit{
+        val imageByteArray = Base64.getDecoder().decode(base64Str)
+        File(pathFile).writeBytes(imageByteArray)
+    }
 }
+
+
