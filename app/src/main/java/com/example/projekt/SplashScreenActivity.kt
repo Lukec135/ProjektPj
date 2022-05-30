e23b783156e783d4fd15a70887578e6e1c9b323c
Mernik <mernik.luka1@gmail.com>
2022-05-30 13:18:11 +0200

    Add SplashScreenActivity.kt

---
+++ app/src/main/java/com/example/projekt/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projekt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var splash:Long=3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+
+                 startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        },splash)
+    }
+}
